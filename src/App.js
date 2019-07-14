07cec059dd626bc79a2d5b7171fdad8f816fd9c8
Vanessa <vanss472@gmail.com>
2019-07-14 11:50:20 -0400

    change board to boardLists, calling list instead of label

--- src/App.js
+++ src/App.js
@@ -4,26 +4,25 @@ import * as Constants from './constants';
 
 class App extends Component {
 	state = {
-		boards: []
+		boardLists: []
 	}
 
 	componentDidMount() {
 		const url = `${Constants.API_URL}/1/boards/${Constants.BOARD_ID}/lists/?fields=name,color,url&key=${Constants.API_KEY}&token=${Constants.API_TOKEN}`;
 		axios.get(url).then(response => response.data)
 		.then((data) => {
-			this.setState({ boards: data })
-			console.log(this.state.boards);
+			this.setState({ boardLists: data })
+			console.log(this.state.boardLists);
 		})
 	}
 
   render() {
 		return (
 			<div className="row small-up-1 medium-up-2 large-up-3">
-				{this.state.boards.map((label) => (
+				{this.state.boardLists.map((list) => (
 					<div className="column">
 						<div className="callout">
-							<p>{label.name}</p>
-							<p>{label.color}</p>
+							<h2>{list.name}</h2>
 						</div>
 					</div>
 				))}
